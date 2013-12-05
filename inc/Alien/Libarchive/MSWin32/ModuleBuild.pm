59adc3d959c238ce4652a4cd4c77404d2a3499f5
Graham Ollis <plicease@cpan.org>
2013-12-05 17:26:15 -0500

    reorg the stuff installed into the share dir

--- inc/Alien/Libarchive/MSWin32/ModuleBuild.pm
+++ inc/Alien/Libarchive/MSWin32/ModuleBuild.pm
@@ -25,14 +25,18 @@ sub alien_build ()
   my $system = 'MinGW Makefiles';
   _system $cmake, -G => $system, "-DCMAKE_MAKE_PROGRAM:PATH=$make", '.';
   _system $make, 'archive';
+  _system $make, 'archive_static';
 }
 
 sub alien_install ()
 {
   my $dir = shift @ARGV;
+  print "> MD $dir\\bin\n";
+  mkdir "$dir/bin";
+  _system "copy", "bin\\libarchive.dll", "$dir\\bin\\libarchive.dll";
   print "> MD $dir\\lib\n";
   mkdir "$dir/lib";
-  _system "copy", "bin\\libarchive.dll", "$dir\\lib\\libarchive.dll";
+  _system "copy", "libarchive\\libarchive_static.a", "$dir\\lib\\libarchive_static.a";
   _system "copy", "libarchive\\libarchive.dll.a", "$dir\\lib\\libarchive.dll.a";
   print "> MD $dir\\include\n";
   mkdir "$dir/include";
